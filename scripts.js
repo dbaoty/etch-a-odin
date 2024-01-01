1b46a8bab626df51f3c14d92cd1ad42276bae68f
dbaoty <84877646+dbaoty@users.noreply.github.com>
2024-01-01 12:54:13 -0500

    complete layout/temp logo

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 const reset = () => location.reload();
 
+const captureScreenshot = async () => {
+  // todo
+};
+
 function colorGridSquare() {
   let r = (Math.floor(Math.random() * 255));
   let g = (Math.floor(Math.random() * 255));
@@ -10,8 +14,14 @@ function colorGridSquare() {
  
 document.body.setAttribute("style", "display: flex; flex-flow: column wrap; align-items: center; background-color: rgb(230, 80, 80)");
 
-const gridContainer = document.createElement("div");
-gridContainer.setAttribute("style", "display: flex; max-width: 960px; border: 10px solid rgb(180, 80, 80)");
+const topSection = document.createElement("div");
+topSection.setAttribute("style", "display: flex; flex-flow: row wrap");
+
+const midSection = document.createElement("div");
+midSection.setAttribute("style", "display: flex; max-width: 960px; border: 10px solid rgb(180, 80, 80)");
+
+const bottomSection = document.createElement("div");
+bottomSection.setAttribute("style", "display: flex; flex-flow: column wrap; margin-top: 20px; align-items: center");
 
 const createGridButton = document.createElement("button");
 createGridButton.setAttribute("style", "padding: 10px; margin: 20px");
@@ -19,7 +29,22 @@ createGridButton.innerText = "Shake 🤝";
 
 createGridButton.addEventListener("click", reset);
 
-document.body.appendChild(createGridButton);
+const createScreenshotButton = document.createElement("button");
+createScreenshotButton.setAttribute("style", "padding: 10px; margin: 20px");
+createScreenshotButton.innerText = "Save 📷";
+
+createScreenshotButton.addEventListener("click", captureScreenshot);
+
+const logoImage = document.createElement("img");
+logoImage.src = "res/logo.png";
+
+const footer = document.createElement("div");
+footer.innerText = "";
+
+topSection.appendChild(createGridButton);
+topSection.appendChild(createScreenshotButton);
+
+document.body.appendChild(topSection);
 
 let size = 0;
 while (size < 1 || size > 100 || isNaN(size)) size = prompt("size of grid: ");
@@ -37,7 +62,12 @@ for (let i = 0; i < size; ++i) {
     gridSquareContainer.appendChild(gridSquare);
   }
 
-  gridContainer.appendChild(gridSquareContainer);
+  midSection.appendChild(gridSquareContainer);
 }
 
-document.body.appendChild(gridContainer);
+bottomSection.appendChild(logoImage);
+bottomSection.appendChild(footer);
+
+document.body.appendChild(midSection);
+document.body.appendChild(bottomSection);
+// document.body.appendChild(createScreenshotButton);
