39a05d5af5772c4125bc575ca76e357682829e65
dbaoty <84877646+dbaoty@users.noreply.github.com>
2024-01-01 08:37:24 -0500

    scripts.js refactor

--- scripts.js
+++ scripts.js
@@ -4,51 +4,17 @@ document.body.style.justifyContent = "center";
 document.body.style.alignItems = "center";
 
 const gridContainer = document.createElement("div");
-gridContainer.style.display = "flex";
-gridContainer.style.flexWrap = "wrap";
+gridContainer.setAttribute("style", "display: flex; flexWrap: wrap");
 
 const testFunction = () => console.log("hello, world");
 
-/*
-
-  generate "gridSquareContainer" and fill it with 16 "gridSquare" elements.
-  push the "gridSquareContainer" into "gridContainer"
-
-  each gridSquareContainer contains 16 gridSquare elements
-  gridSquareContainer contains 16 gridSquareContainers
-
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-  [][][][][][][][][][][][][][][][]
-
-*/
-
 for (let i = 0; i < 16; ++i) {
   let gridSquareContainer = document.createElement("div");
-  gridSquareContainer.style.flexDirection = "row";
+  gridSquareContainer.setAttribute("style", "flexDirection: row");
 
   for (let j = 0; j < 16; ++j) {
     let gridSquare = document.createElement("div");
-
-    gridSquare.style.boxSizing = "border-box";
-    gridSquare.style.width = "20px";
-    gridSquare.style.height = "20px";
-
-    gridSquare.style.border = "2px solid gray";
-
+    gridSquare.setAttribute("style", "boxSizing: border-box; width: 20px; height: 20px; border: 2px solid gray");
     gridSquare.addEventListener("mouseover", testFunction);
 
     gridSquareContainer.appendChild(gridSquare);
