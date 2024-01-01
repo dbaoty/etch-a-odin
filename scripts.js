cff131998e95f320d6bc919503cf7e7604e54918
dbaoty <84877646+dbaoty@users.noreply.github.com>
2024-01-01 09:15:16 -0500

    ability to paint gridSquare div on mouse hover

--- scripts.js
+++ scripts.js
@@ -6,7 +6,10 @@ document.body.style.alignItems = "center";
 const gridContainer = document.createElement("div");
 gridContainer.setAttribute("style", "display: flex; flexWrap: wrap");
 
-const testFunction = () => console.log("hello, world");
+function testFunction() {
+  console.log(this);
+  this.setAttribute("style", "boxSizing: border-box; width: 20px; height: 20px; background-color: black; border: 1px solid black;")
+}
 
 for (let i = 0; i < 16; ++i) {
   let gridSquareContainer = document.createElement("div");
@@ -14,7 +17,7 @@ for (let i = 0; i < 16; ++i) {
 
   for (let j = 0; j < 16; ++j) {
     let gridSquare = document.createElement("div");
-    gridSquare.setAttribute("style", "boxSizing: border-box; width: 20px; height: 20px; border: 2px solid gray");
+    gridSquare.setAttribute("style", "boxSizing: border-box; width: 20px; height: 20px; border: 1px solid gray");
     gridSquare.addEventListener("mouseover", testFunction);
 
     gridSquareContainer.appendChild(gridSquare);
