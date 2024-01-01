fb5a99b9eb2d0d1dbccac2c30811e9bb6248299a
dbaoty <84877646+dbaoty@users.noreply.github.com>
2024-01-01 12:03:32 -0500

    basic etch-a-sketch look and functionality

--- scripts.js
+++ scripts.js
@@ -1,24 +1,38 @@
+const reset = () => location.reload();
 
-document.body.style.display = "flex";
-document.body.style.justifyContent = "center";
-document.body.style.alignItems = "center";
+function colorGridSquare() {
+  let r = (Math.floor(Math.random() * 255));
+  let g = (Math.floor(Math.random() * 255));
+  let b = (Math.floor(Math.random() * 255));
+  
+  this.setAttribute("style", `border: 4px solid rgb(${r}, ${g}, ${b})`)
+}
+ 
+document.body.setAttribute("style", "display: flex; flex-flow: column wrap; align-items: center; background-color: rgb(230, 80, 80)");
 
 const gridContainer = document.createElement("div");
-gridContainer.setAttribute("style", "display: flex; flexWrap: wrap");
+gridContainer.setAttribute("style", "display: flex; max-width: 960px; border: 10px solid rgb(180, 80, 80)");
 
-function testFunction() {
-  console.log(this);
-  this.setAttribute("style", "boxSizing: border-box; width: 20px; height: 20px; background-color: black; border: 1px solid black;")
-}
+const createGridButton = document.createElement("button");
+createGridButton.setAttribute("style", "padding: 10px; margin: 20px");
+createGridButton.innerText = "Shake 🤝";
 
-for (let i = 0; i < 16; ++i) {
+createGridButton.addEventListener("click", reset);
+
+document.body.appendChild(createGridButton);
+
+let size = 0;
+while (size < 1 || size > 100 || isNaN(size)) size = prompt("size of grid: ");
+
+for (let i = 0; i < size; ++i) {
   let gridSquareContainer = document.createElement("div");
-  gridSquareContainer.setAttribute("style", "flexDirection: row");
+  gridSquareContainer.setAttribute("style", "display: flex; flex-flow: column wrap");
 
-  for (let j = 0; j < 16; ++j) {
+  for (let j = 0; j < size; ++j) {
     let gridSquare = document.createElement("div");
-    gridSquare.setAttribute("style", "boxSizing: border-box; width: 20px; height: 20px; border: 1px solid gray");
-    gridSquare.addEventListener("mouseover", testFunction);
+    gridSquare.setAttribute("style", "display: flex; flex-flow: row wrap; border: 4px solid rgb(240, 240, 240)");
+
+    gridSquare.addEventListener("mouseover", colorGridSquare);
 
     gridSquareContainer.appendChild(gridSquare);
   }
